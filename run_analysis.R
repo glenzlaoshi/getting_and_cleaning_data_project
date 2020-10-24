7920f7b4ed136a8c73f2d2c8bc9db6925ae7339d
glenzlaoshi <70584366+glenzlaoshi@users.noreply.github.com>
2020-10-24 18:33:13 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,6 @@
+# This script is part of the final project for the 'Getting and Cleaning Data' course. 
+# The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set.
+# The goal is to prepare tidy data that can be used for later analysis. 
 
 
 
