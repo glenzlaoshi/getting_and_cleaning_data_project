ce9e4dcbf307bf14a25517313a981347e26ea76f
glenzlaoshi <glenzlaoshi@gmail.com>
2020-10-24 15:21:01 +0000

    finished 'run_analysis', updated others

--- run_analysis.R
+++ run_analysis.R
@@ -1,27 +1,54 @@
 
 
 
+xTrain <- read.table("train/X_train.txt")
+yTrain <- read.table("train/y_train.txt")
+subjectTrain <- read.table("train/subject_train.txt")
+
+xTest <- read.table("test/X_test.txt")
+yTest <- read.table("test/y_test.txt")
+subjectTest <- read.table("test/subject_test.txt")
+
+xData <- rbind(xTrain,xTest)
+yData <- rbind(yTrain,yTest)
+subjectData <- rbind(subjectTrain,subjectTest)
+
+
+
+features <- read.table("features.txt")
+
+selected_features <- grep("-(mean|std)\\(\\)", features[, 2])
+
+xData <- xData[,selected_features]
+
+names(xData) <- features[selected_features,2]
 
 
 
 
+activities <- read.table("activity_labels.txt")
+yData[,1]<-activities[yData[,1],2]
+names(yData)<- c("activity")
+
+names(subjectData)<-c("subject")
+
+cleanData <- cbind(xData,yData,subjectData)
+
+newNames <- colnames(cleanData)
+
+newNames <- gsub("^f","frequency",newNames)
+newNames <- gsub("^t","time",newNames) 
+newNames <- gsub("Acc","Accelerometer",newNames )
+newNames <- gsub("Gyro","Gyroscope",newNames )
+newNames <- gsub("Mag","Magnitude",newNames )
+newNames <- gsub("Freq","Frequency",newNames )
+newNames <- gsub("std","StandardDeviation",newNames) 
+
+colnames(cleanData) <- newNames
 
 
+averageData<-cleanData %>%
+	group_by(subject,activity) %>%
+	summarize(across(.cols=everything(),.fns=list(mean=mean)))
 
-
-# add something to load packages
-
-
-# Get the data
-
-labels <- read.table("UCI HAR Dataset/activity_labels.txt",col.names=c("id","activity"))
-labels[,2] <- as.character(labels[,2])
-
-features <- read.table("UCI HAR Dataset/features.txt",col.names=c("id","featureType"))
-features[,2] <- as.character(features[,2])
-
-needed_features<-grep("(mean|std)\\(\\)",features[,2])
-needed_features_names<-features[needed_features,2]
-
-
-
+write.table(averageData, "cleaned_and_averaged_data.txt")
